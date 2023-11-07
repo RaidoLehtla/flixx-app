4882b834921ddfedf548c6421583924d5a933fa1
TheRaidoX <raidolehtla@live.com>
2023-11-07 07:51:40 +0200

    shows html update

--- js/script.js
+++ js/script.js
@@ -8,12 +8,11 @@ const global = {
     totalResults: 0,
   },
   api: {
-    apiKey: '3fd2be6f0c70a2a598f084ddfb75487c',
+    apiKey: 'd2788025add102a7b7100b747cb046af',
     apiUrl: 'https://api.themoviedb.org/3/',
   },
 };
 
-// Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
 
@@ -22,19 +21,19 @@ async function displayPopularMovies() {
     div.classList.add('card');
     div.innerHTML = `
           <a href="movie-details.html?id=${movie.id}">
-            ${
-              movie.poster_path
-                ? `<img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-              class="card-img-top"
-              alt="${movie.title}"
-            />`
-                : `<img
-            src="../images/no-image.jpg"
+           ${
+             movie.poster_path
+               ? ` <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
             class="card-img-top"
             alt="${movie.title}"
           />`
-            }
+               : ` <img
+               src="images/no-image.jpg"
+               class="card-img-top"
+               alt="${movie.title}"
+             />`
+           }
           </a>
           <div class="card-body">
             <h5 class="card-title">${movie.title}</h5>
@@ -42,7 +41,7 @@ async function displayPopularMovies() {
               <small class="text-muted">Release: ${movie.release_date}</small>
             </p>
           </div>
-        `;
+    `;
 
     document.querySelector('#popular-movies').appendChild(div);
   });
@@ -57,19 +56,19 @@ async function displayPopularShows() {
     div.classList.add('card');
     div.innerHTML = `
           <a href="tv-details.html?id=${show.id}">
-            ${
-              show.poster_path
-                ? `<img
-              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-              class="card-img-top"
-              alt="${show.name}"
-            />`
-                : `<img
-            src="../images/no-image.jpg"
+           ${
+             show.poster_path
+               ? ` <img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
             class="card-img-top"
             alt="${show.name}"
           />`
-            }
+               : ` <img
+               src="images/no-image.jpg"
+               class="card-img-top"
+               alt="${show.name}"
+             />`
+           }
           </a>
           <div class="card-body">
             <h5 class="card-title">${show.name}</h5>
@@ -77,7 +76,7 @@ async function displayPopularShows() {
               <small class="text-muted">Air Date: ${show.first_air_date}</small>
             </p>
           </div>
-        `;
+    `;
 
     document.querySelector('#popular-shows').appendChild(div);
   });
@@ -93,64 +92,64 @@ async function displayMovieDetails() {
   displayBackgroundImage('movie', movie.backdrop_path);
 
   const div = document.createElement('div');
-
   div.innerHTML = `
   <div class="details-top">
-  <div>
-  ${
-    movie.poster_path
-      ? `<img
-    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-    class="card-img-top"
-    alt="${movie.title}"
-  />`
-      : `<img
-  src="../images/no-image.jpg"
-  class="card-img-top"
-  alt="${movie.title}"
-/>`
-  }
-  </div>
-  <div>
-    <h2>${movie.title}</h2>
-    <p>
-      <i class="fas fa-star text-primary"></i>
-      ${movie.vote_average.toFixed(1)} / 10
-    </p>
-    <p class="text-muted">Release Date: ${movie.release_date}</p>
-    <p>
-      ${movie.overview}
-    </p>
-    <h5>Genres</h5>
-    <ul class="list-group">
-      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
-    </ul>
-    <a href="${
-      movie.homepage
-    }" target="_blank" class="btn">Visit Movie Homepage</a>
-  </div>
-</div>
-<div class="details-bottom">
-  <h2>Movie Info</h2>
-  <ul>
-    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
-      movie.budget
-    )}</li>
-    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
-      movie.revenue
-    )}</li>
-    <li><span class="text-secondary">Runtime:</span> ${
-      movie.runtime
-    } minutes</li>
-    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
-  </ul>
-  <h4>Production Companies</h4>
-  <div class="list-group">
-    ${movie.production_companies
-      .map((company) => `<span>${company.name}</span>`)
-      .join(', ')}
-  </div>
-</div>
+          <div>
+          ${
+            movie.poster_path
+              ? ` <img
+           src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+           class="card-img-top"
+           alt="${movie.title}"
+         />`
+              : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+             ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span>  $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}
+            </li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${movie.production_companies
+            .map((company) => `<span>${company.name}</span>`)
+            .join(', ')}
+          </div>
+        </div>
   `;
 
   document.querySelector('#movie-details').appendChild(div);
@@ -166,61 +165,61 @@ async function displayShowDetails() {
   displayBackgroundImage('tv', show.backdrop_path);
 
   const div = document.createElement('div');
-
   div.innerHTML = `
   <div class="details-top">
-  <div>
-  ${
-    show.poster_path
-      ? `<img
-    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-    class="card-img-top"
-    alt="${show.name}"
-  />`
-      : `<img
-  src="../images/no-image.jpg"
-  class="card-img-top"
-  alt="${show.name}"
-/>`
-  }
-  </div>
-  <div>
-    <h2>${show.name}</h2>
-    <p>
-      <i class="fas fa-star text-primary"></i>
-      ${show.vote_average.toFixed(1)} / 10
-    </p>
-    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
-    <p>
-      ${show.overview}
-    </p>
-    <h5>Genres</h5>
-    <ul class="list-group">
-      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
-    </ul>
-    <a href="${
-      show.homepage
-    }" target="_blank" class="btn">Visit show Homepage</a>
-  </div>
-</div>
-<div class="details-bottom">
-  <h2>Show Info</h2>
-  <ul>
-    <li><span class="text-secondary">Number of Episodes:</span> ${
-      show.number_of_episodes
-    }</li>
-    <li><span class="text-secondary">Last Episode To Air:</span> ${
-      show.last_episode_to_air.name
-    }</li>
-    <li><span class="text-secondary">Status:</span> ${show.status}</li>
-  </ul>
-  <h4>Production Companies</h4>
-  <div class="list-group">
-    ${show.production_companies
-      .map((company) => `<span>${company.name}</span>`)
-      .join(', ')}
-  </div>
-</div>
+          <div>
+          ${
+            show.poster_path
+              ? ` <img
+           src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+           class="card-img-top"
+           alt="${show.name}"
+         />`
+              : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>
+             ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li><span class="text-secondary">Last Episode To Air:</span> ${
+              show.last_episode_to_air.name
+            }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies
+            .map((company) => `<span>${company.name}</span>`)
+            .join(', ')}
+          </div>
+        </div>
   `;
 
   document.querySelector('#show-details').appendChild(div);
@@ -287,23 +286,21 @@ function displaySearchResults(results) {
     div.classList.add('card');
     div.innerHTML = `
           <a href="${global.search.type}-details.html?id=${result.id}">
-            ${
-              result.poster_path
-                ? `<img
-              src="https://image.tmdb.org/t/p/w500${result.poster_path}"
-              class="card-img-top"
-              alt="${
-                global.search.type === 'movie' ? result.title : result.name
-              }"
-            />`
-                : `<img
-            src="../images/no-image.jpg"
+           ${
+             result.poster_path
+               ? ` <img
+            src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
             class="card-img-top"
-             alt="${
-               global.search.type === 'movie' ? result.title : result.name
-             }"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
           />`
-            }
+               : ` <img
+               src="images/no-image.jpg"
+               class="card-img-top"
+               alt="${
+                 global.search.type === 'movie' ? result.title : result.name
+               }""
+             />`
+           }
           </a>
           <div class="card-body">
             <h5 class="card-title">${
@@ -317,10 +314,10 @@ function displaySearchResults(results) {
               }</small>
             </p>
           </div>
-        `;
+    `;
 
     document.querySelector('#search-results-heading').innerHTML = `
-              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
     `;
 
     document.querySelector('#search-results').appendChild(div);
@@ -329,14 +326,14 @@ function displaySearchResults(results) {
   displayPagination();
 }
 
-// Create & Display Pagination For Search
+// Create and Display Pagination For Search
 function displayPagination() {
   const div = document.createElement('div');
   div.classList.add('pagination');
   div.innerHTML = `
-  <button class="btn btn-primary" id="prev">Prev</button>
-  <button class="btn btn-primary" id="next">Next</button>
-  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+   <button class="btn btn-primary" id="prev">Prev</button>
+   <button class="btn btn-primary" id="next">Next</button>
+   <div class="page-counter">${global.search.page} of ${global.search.totalPages}</div>
   `;
 
   document.querySelector('#pagination').appendChild(div);
@@ -375,12 +372,12 @@ async function displaySlider() {
     div.classList.add('swiper-slide');
 
     div.innerHTML = `
-      <a href="movie-details.html?id=${movie.id}">
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
-      </a>
-      <h4 class="swiper-rating">
-        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
-      </h4>
+    <a href="movie-details.html?id=${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+    </h4>
     `;
 
     document.querySelector('.swiper-wrapper').appendChild(div);
@@ -432,7 +429,7 @@ async function fetchAPIData(endpoint) {
 }
 
 // Make Request To Search
-async function searchAPIData() {
+async function searchAPIData(endpoint) {
   const API_KEY = global.api.apiKey;
   const API_URL = global.api.apiUrl;
 
